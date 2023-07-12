29976280bd843206e5bc5c617cc66e289cc10847
michael <michael.bodjolle@laplateforme.io>
2023-07-12 16:32:37 +0200

    Jquery header

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// $(function() {
+//     $("#header").load("header.html");
+//   });
 
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
 
-document.querySelectorAll(".nav-link").forEach(n => 
-    n.addEventListener("click", () =>{
-        hamburger.classList.remove("active")
-        navMenu.classList.remove("active")
-    })
-    )
+
+
+// hamburger.addEventListener("click", () =>{
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// })
+
+// document.querySelectorAll(".nav-link").forEach(n => 
+//     n.addEventListener("click", () =>{
+//         hamburger.classList.remove("active")
+//         navMenu.classList.remove("active")
+//     })
+//     )
+
+$(function() {
+    // Charger le contenu du header
+    $("#header").load("header.html", function() {
+      // Gestion du menu burger
+      $(".hamburger").on("click", function() {
+        $(this).toggleClass("active");
+        $(".nav-menu").toggleClass("active");
+      });
+  
+      $(".nav-link").on("click", function() {
+        $(".hamburger").removeClass("active");
+        $(".nav-menu").removeClass("active");
+      });
+    });
+  });
