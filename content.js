58d881114a7b4ba727ff7aabc28e365f0b761e22
Utilisateur invité <you@example.com>
2023-07-20 16:00:39 +0200

    part content second

--- content.js
+++ content.js
@@ -1,9 +1,16 @@
 window.addEventListener('load', (e) => {
 
-    e.preventDefault();
+    // e.preventDefault();
 
     const box = document.querySelector("#content");
+    const img = document.createElement("img");
+
+    img.src = "images/VALID-HH-13.JPG";
+    img.classList.add("imgone");
     
+    const src = document.getElementById("myImg");
+
+    src.appendChild(img);
 
     
 
