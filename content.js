9cb37b56e99220a41254c823ec4473d671a032b6
Utilisateur invité <you@example.com>
2023-07-20 17:28:35 +0200

    minblock

--- content.js
+++ content.js
@@ -4,13 +4,13 @@ window.addEventListener('load', (e) => {
 
     const box = document.querySelector("#content");
     const img = document.createElement("img");
-
+    
     img.src = "images/VALID-HH-13.JPG";
     img.classList.add("imgone");
     
     const src = document.getElementById("myImg");
-
     src.appendChild(img);
+    // const boxtwo = document.getElementById();
 
     
 
