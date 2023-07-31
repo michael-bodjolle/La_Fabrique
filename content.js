b2cf86a95331db658b8f8c5cd0ad2435906f7977
Mike <you@example.com>
2023-07-31 15:00:22 +0200

    mediaquerrysetting

--- content.js
+++ content.js
@@ -4,14 +4,22 @@ window.addEventListener('load', (e) => {
 
     const box = document.querySelector("#content");
     const img = document.createElement("img");
-    
+    const imgTwo = document.createElement("img");
+
+
     img.src = "images/SHOW-28.JPG";
     img.classList.add("imgone");
-    
+
     const src = document.getElementById("myImg");
     src.appendChild(img);
-    // const boxtwo = document.getElementById();
 
-    
+
+
+
+    imgTwo.src = "images/sourir.JPG";
+    imgTwo.classList.add("imgtwo");
+
+    const src2 = document.getElementById("myimgtwo");
+    src2.appendChild(imgTwo);
 
 })
