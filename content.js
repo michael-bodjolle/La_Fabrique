cffc9196f09ac44d48f6a6dd1d46596319fad394
michael-bodjolle <michael.bodjolle@laplateforme.io>
2023-08-01 23:21:49 +0200

    scrolldonet

--- content.js
+++ content.js
@@ -3,12 +3,12 @@ window.addEventListener('load', (e) => {
     // e.preventDefault();
 
     
-
-    const box = document.querySelector("#content");
+    const box_el =document.querySelector("#box_el");
+    const box = document.querySelector("#content"); 
     const img = document.createElement("img");
     const imgTwo = document.createElement("img");
-
-
+   
+    
     img.src = "images/SHOW-28.JPG";
     img.classList.add("imgone");
 
@@ -23,6 +23,22 @@ window.addEventListener('load', (e) => {
 
     const src2 = document.getElementById("myimgtwo");
     src2.appendChild(imgTwo);
+     
+    
+    // box1
+    const elem1 = document.createElement("div");
+    elem1.classList.add("elem1");
+
+    box_el.appendChild(elem1);
+    console.log(elem1);
+    // box2
+    const elem2 = document.createElement("div");
+    elem2.classList.add("elem2");
+   
+
+    box_el.appendChild(elem2);
+    console.log(elem2);
+
   
 
    
